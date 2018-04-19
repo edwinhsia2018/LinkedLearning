a63b7e06952b1121209b6a116474b3e17702e7d6
Edwin Hsia <edwinhsia@gmail.com>
2018-04-19 14:30:09 -0500

    Removing excess comments

--- server.js
+++ server.js
@@ -1,14 +1,7 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
 var express = require("express");
 var bodyParser = require("body-parser");
 
 // Sets up the Express App
-// =============================================================
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -21,12 +14,10 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // Routes
-// =============================================================
 require("./routes/link-api-routes.js")(app);
 require("./routes/author-api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
-// =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
